a77568cc8d93853003d0d228168fc103ca9bd1f9
Rizaldy Iman Putra <rizaldyimanputra@gmail.com>
2024-06-13 13:13:27 +0700

    initial commit

---
+++ src/test/java/com/pwdk/minpro_be/MinproBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pwdk.minpro_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinproBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
