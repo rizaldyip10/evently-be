fbcf41d09e713877e6984e3fc32e3eddd51d999f
Rizaldy Iman Putra <rizaldyimanputra@gmail.com>
2024-07-17 01:34:42 +0700

    feat(claims): revert claims and profile endpoint to initial (#22)
    * feat(auth profile): debugging profile endpoint so it won't return error 401 after login
    
    * feat(auth): debugging cookie so the app can recieve the cookie
    
    * feat(cors) debugging cors origin pattern
    
    * feat(cookie): adding secure to cookie setting
    
    * feat(dockerfile): adding expose the port
    
    * feat(auth): setting back token expire
    
    * feat(claims): revert claims and profile endpoint to initial

--- src/main/java/com/pwdk/minpro_be/auth/helpers/Claims.java
+++ src/main/java/com/pwdk/minpro_be/auth/helpers/Claims.java
@@ -4,24 +4,16 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
-import java.util.Collections;
 import java.util.Map;
+import java.util.Objects;
 
 public class Claims {
 
-    public static Map<String, Object> getClaimsFromJwt() {
+    public static Map<String, Object> getClaimsFromJwt(){
         SecurityContext context = SecurityContextHolder.getContext();
         Authentication authentication = context.getAuthentication();
-
-        if (authentication instanceof JwtAuthenticationToken) {
-            return ((JwtAuthenticationToken) authentication).getToken().getClaims();
-        } else if (authentication.getPrincipal() instanceof Jwt) {
-            return ((Jwt) authentication.getPrincipal()).getClaims();
-        } else {
-            // If it's neither JwtAuthenticationToken nor Jwt, return an empty map or handle as needed
-            return Collections.emptyMap();
-        }
+        Jwt jwt = (Jwt) authentication.getPrincipal();
+        return jwt.getClaims();
     }
 }
