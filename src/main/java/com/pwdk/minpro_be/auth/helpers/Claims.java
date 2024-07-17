0754b8d9dccb3a01a561f9dc271e5e65826b2dfd
Rizaldy Iman Putra <rizaldyimanputra@gmail.com>
2024-07-17 08:05:01 +0700

    feat(log): adding log to see what went wrong

--- src/main/java/com/pwdk/minpro_be/auth/helpers/Claims.java
+++ src/main/java/com/pwdk/minpro_be/auth/helpers/Claims.java
@@ -1,19 +1,30 @@
 package com.pwdk.minpro_be.auth.helpers;
 
+import lombok.extern.java.Log;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
+import java.util.Collections;
 import java.util.Map;
-import java.util.Objects;
 
+@Log
 public class Claims {
 
-    public static Map<String, Object> getClaimsFromJwt(){
+    public static Map<String, Object> getClaimsFromJwt() {
         SecurityContext context = SecurityContextHolder.getContext();
+        log.info("Security context -> " + context.toString());
         Authentication authentication = context.getAuthentication();
-        Jwt jwt = (Jwt) authentication.getPrincipal();
-        return jwt.getClaims();
+
+        if (authentication instanceof JwtAuthenticationToken) {
+            return ((JwtAuthenticationToken) authentication).getToken().getClaims();
+        } else if (authentication.getPrincipal() instanceof Jwt) {
+            return ((Jwt) authentication.getPrincipal()).getClaims();
+        } else {
+            // If it's neither JwtAuthenticationToken nor Jwt, return an empty map or handle as needed
+            return Collections.emptyMap();
+        }
     }
 }
