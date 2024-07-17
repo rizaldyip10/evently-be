c20dc80a5abd6fee2b308e91d196a036149e1972
Rizaldy Iman Putra <rizaldyimanputra@gmail.com>
2024-07-17 13:01:31 +0700

    feat(profile dto): adding points to profile response dto

--- src/main/java/com/pwdk/minpro_be/auth/dto/ProfileResponseDto.java
+++ src/main/java/com/pwdk/minpro_be/auth/dto/ProfileResponseDto.java
@@ -1,5 +1,6 @@
 package com.pwdk.minpro_be.auth.dto;
 
+import com.pwdk.minpro_be.point.dto.PointResponseDto;
 import com.pwdk.minpro_be.roles.entity.Roles;
 import lombok.Data;
 
@@ -9,4 +10,5 @@ public class ProfileResponseDto {
     private String email;
     private String profileImg;
     private Roles role;
+    private PointResponseDto points;
 }
